4bb5ee215703d124fa2c2379cc1c256f5daa1eb9
mvinchankar <mvinchankar18@gmail.com>
2019-12-28 16:30:04 +0530

    Refactore And added code for user email validation.

--- userRegClient.js
+++ userRegClient.js
@@ -1,24 +1,22 @@
+var readlineSync = require('readline-sync');
 let userReg = require('./userRegistration');
+ 
+var uFname = readlineSync.question('Enter the first name : ');
+if (userReg.firstName(uFname))
+	console.log("Valid");	
+else
+	console.log("Invalid");
 
-let good1 = userReg.firstName("Ravi");
-console.log(good1);
-let err1 = userReg.firstName("ravi");
-console.log(err1);
-let err2 = userReg.firstName("Ravi1");
-console.log(err2);
-let err3 = userReg.firstName("1Ravi");
-console.log(err3);
-let err4 = userReg.firstName("Ravi@");
-console.log(err4);
-console.log("\n");
-let good2 = userReg.lastName("Gaikwad");
-console.log(good2);
-let err5 = userReg.lastName("Gaikwad1");
-console.log(err5);
-let err6 = userReg.lastName("1Gaikwad");
-console.log(err6);
-let err7 = userReg.lastName("gaikwad");
-console.log(err7);
-let err8 = userReg.lastName("Gaikwad@");
-console.log(err8);
+var uLname = readlineSync.question('Enter the last name : ');
+if(userReg.lastName(uLname))
+	console.log("Valid");	
+else
+	console.log("Invalid");
+
+var uEmail = readlineSync.question('Enter the email : ');
+if (userReg.checkvalidEmail(uEmail))
+	console.log("Valid");	
+else
+	console.log("Invalid");
+
 
--- userRegistration.js
+++ userRegistration.js
@@ -9,5 +9,10 @@ class userRegistration{
 	let pat = RegExp('^[A-Z]{1}[a-z]{3,}$');
 	return pat.test(name);
 	}
+
+	checkvalidEmail(email){
+	let pat = RegExp('^[a-zA-Z0-9]([._+-]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}$');
+	return pat.test(email);
+	}
 }
 module.exports = new userRegistration;
